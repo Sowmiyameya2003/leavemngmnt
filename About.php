68efe27859cc0f3cc519924b34a52c023610646d
Sowmiyameya2003 <116428482+Sowmiyameya2003@users.noreply.github.com>
2022-10-22 21:45:28 +0530

    Create About.php

---
+++ About.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include("connection.php");
+?>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="style.css"/>
+        <title>Home page</title>
+      
+    </head>
+    <body>
+    <div class="background-image"></div>
+<div class="navbar"> 
+
+    <img src="logo.png">
+    <p> iLeave</p>
+</body>
+</html>
